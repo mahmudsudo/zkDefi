721409cdc6cfe493e87e115e4f3ef6c523ffa984
= <=>
2024-07-23 00:09:07 +0100

    lending position structure added

---
+++ contracts/src/Lend.test.ts
@@ -0,0 +1,7 @@
+// import { lend } from './lend';
+
+describe('lend.js', () => {
+  describe('lend()', () => {
+    it.todo('should be correct');
+  });
+});
---
+++ contracts/src/Lend.ts
@@ -0,0 +1,29 @@
+import {
+    Field,
+    SmartContract,
+    state,
+    State,
+    method,
+    MerkleMap,
+    Struct,
+} from 'o1js';
+
+export class Lend extends SmartContract {
+    @state(Field) num = State<Field>();
+
+    init() {
+           super.init();
+           this.num.set(Field(3));
+    }
+    @method async update(square: Field) {
+             const currentState = this.num.get();
+             this.num.requireEquals(currentState);
+             square.assertEquals(currentState.mul(currentState));
+             this.num.set(square);
+           }
+
+}
+
+class Position extends Struct({amount : Field , realized_pnl :Field}){
+    Position(amount :Field, realized_pnl:Field ){}
+}
